6a72100767ee362bd43f97925b71a9a7694fa621
Kfirm1208 <106590971+Kfirm1208@users.noreply.github.com>
2023-01-24 00:33:32 +0700

    commit

--- lab3/ex3.py
+++ lab3/ex3.py
@@ -1,9 +1,7 @@
 def is_plusone_dictionary(dict1):
-   for i,j in zip(dict1.keys(),dict1.values()):
-       if dict1[i] < dict1[i+2] and dict1[i] < dict1[i]+2:
-           return True
-       else:
-           return False
+    for i in dict1:
+        x = dict1.get(i)
+        
 
 print(is_plusone_dictionary({1:2, 3:4, 5:6, 7:8}))      
            
