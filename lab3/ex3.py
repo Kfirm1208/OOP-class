9d2bd4f204b32171b3a3deb51c94cb55c08e1ce7
Kfirm1208 <106590971+Kfirm1208@users.noreply.github.com>
2023-01-24 11:30:21 +0700

    commit

--- lab3/ex3.py
+++ lab3/ex3.py
@@ -1,9 +1,30 @@
-def is_plusone_dictionary(d):
-    keys = sorted(d.keys())
-    for i in range(len(keys)):
-        if d[keys[i]] - keys[i] != 1 :
-            return False
-    return True
+# def is_plusone_dictionary(d):
+#     keys = sorted(d.keys())
+#     for i in range(len(keys)):
+#         if d[keys[i]] - keys[i] != 1 :
+#             return False
+#     return True
 
-print(is_plusone_dictionary({2:3,4:5,6:7,8:9}))
+# def is_plusone_dictionary_v4(d):
+#     return all(key+1 == value for key,value in d.items())
+
+# def is_plusone_dictionary_v5(d):
+#     return all(key+1 == value for key, value in zip(d.keys(), d.values()))
+
+# def is_plusone_dictionary_v6(d):
+#     keys = list(d.keys())
+#     values = list(d.values())
+#     keys.sort()
+#     values.sort()
+#     for i in range(len(keys)):
+#         if keys[i] + 1 != values[i]:
+#             return False
+#     return True
+def is_plusone_dictionary_v7(d):
+    keys = list(d.keys())
+    values = list(d.values())
+    diff = list(map(lambda x, y: x+1 == y, keys, values))
+    return all(diff)
+
+print(is_plusone_dictionary_v7({2:3,4:5,6:7,8:9}))
            
